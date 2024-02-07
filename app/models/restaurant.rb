bdd5ea94a7a137173207e9aca5db9c025a79ced7
Samuel Anderson <samuelSD.999@gmail.com>
2024-02-07 04:16:46 +0000

    Added restaurant and review models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian] }
+  validates :name, presence: true
+  validates :address, presence: true
+  has_many :reviews, dependent: :destroy
 end
---
+++ db/migrate/20240206205246_add_foreign_key_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToReviews < ActiveRecord::Migration[7.1]
+  def change
+    add_column :reviews, :restaurant_id, :integer
+  end
+end
